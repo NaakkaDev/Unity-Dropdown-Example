88cfc547966ca37e270d2078d951005aef898371
Temeez <temeez.dev@gmail.com>
2016-11-08 13:59:26 +0200

    Initial commit

---
+++ Assets/Scripts/UIDropdown.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDropdown : MonoBehaviour {
+    [Tooltip("Add items (gameobjects) to the dropdown which has the given tag.")]
+    public string populateByTag;
+
+    Dropdown m_dropdown;
+
+    void Awake() {
+        m_dropdown = gameObject.GetComponent<Dropdown>();
+        m_dropdown.ClearOptions();
+
+        GameObject[] tag_items = GameObject.FindGameObjectsWithTag(populateByTag);
+        if (populateByTag != null) {
+            foreach (GameObject item in tag_items) {
+                AddItemToDropdown(item.name);
+            }
+        }
+
+        m_dropdown.RefreshShownValue();
+
+        m_dropdown.onValueChanged.AddListener(delegate {
+            print("Item: " + m_dropdown.options[m_dropdown.value].text + " | Id: " + m_dropdown.value);
+        });
+    }
+
+    public void SelectOption(string text) {
+        int option_id = 0;
+        foreach (Dropdown.OptionData data in m_dropdown.options) {
+            if (data.text == text) {
+                m_dropdown.value = option_id;
+                m_dropdown.RefreshShownValue();
+            }
+            option_id++;
+        }
+    }
+
+    public void AddItemToDropdown(string item, bool select_on_add=false) {
+        m_dropdown.options.Add(new Dropdown.OptionData(item));
+
+        if (select_on_add) {
+            SelectOption(item);
+        }
+    }
+
+    public void AddItemToDropdownAndSelect(string item) {
+        AddItemToDropdown(item, true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity 5 Simple Dropdown Example
+
+Simple example on how to add items to the UI dropdown list programmatically with C#. Script finds all gameobjects with a certain tag and adds them to the dropdown.
+
+Download and open this with Unity >= 5 to see how it works.
